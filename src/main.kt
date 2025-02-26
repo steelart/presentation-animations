a7138c5095ef69de73f98a0eded58d1556219358
Alexey.Merkulov <alexey.merkulov@jetbrains.com>
2025-02-26 19:00:24 +0100

    in-progress

--- src/main.kt
+++ src/main.kt
@@ -9,16 +9,37 @@ import korlibs.io.file.std.*
 import korlibs.math.geom.*
 import korlibs.math.interpolation.*
 
-suspend fun main() = Korge(windowSize = Size(512, 512), backgroundColor = Colors["#2b2b2b"]) {
+
+val windowSize = Size(512, 512)
+const val threadHeight = 100.0
+
+
+suspend fun main() = Korge(windowSize = windowSize, backgroundColor = Colors["#2b2b2b"]) {
     val sceneContainer = sceneContainer()
+
 
     sceneContainer.changeTo { MyScene() }
 }
 
+
 class MyScene : Scene() {
     override suspend fun SContainer.sceneMain() {
+        val firstThreadY = 100.0
+
+        solidRect(windowSize.width, threadHeight, Colors.BLUEVIOLET) {
+            y = firstThreadY
+        }
+
+
+        val chunkWidth = 200.0
+        val chunk = roundRect(Size(chunkWidth, threadHeight), RectCorners(5.0), fill = Colors.ORANGE)
+        chunk.y = firstThreadY
+        chunk.tween(chunk::x[windowSize.width, 0.0 - chunkWidth], time = 5.seconds, easing = Easing.LINEAR)
+
+
         val minDegrees = (-16).degrees
         val maxDegrees = (+16).degrees
+
 
         val image = image(resourcesVfs["korge.png"].readBitmap()) {
             rotation = maxDegrees
@@ -26,6 +47,7 @@ class MyScene : Scene() {
             scale(0.8)
             position(256, 256)
         }
+
 
         while (true) {
             image.tween(image::rotation[minDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
