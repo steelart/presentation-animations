900f2555623958a5d554fb9c241e86af86a32911
Alexey.Merkulov <alexey.merkulov@jetbrains.com>
2025-02-26 22:26:02 +0100

    in-progress-3

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,4 @@
+import korlibs.event.Key
 import korlibs.time.*
 import korlibs.korge.*
 import korlibs.korge.scene.*
@@ -5,21 +6,25 @@ import korlibs.korge.tween.*
 import korlibs.korge.view.*
 import korlibs.image.color.*
 import korlibs.image.format.*
+import korlibs.image.paint.GradientPaint
+import korlibs.image.paint.LinearGradientPaint
 import korlibs.image.text.TextAlignment.Companion.MIDDLE_LEFT
 import korlibs.io.file.std.*
+import korlibs.korge.input.keys
 import korlibs.korge.view.roundRect
+import korlibs.korge.view.vector.gpuShapeView
 import korlibs.math.geom.*
 import korlibs.math.interpolation.*
 
 
-val windowSize = Size(512, 512)
-const val threadHeight = 100.0
+val windowSize = Size(512, 512)*2
+val threadHeight = windowSize.height/5
 
-const val textSize = threadHeight / 2
-const val textXShift = textSize/4
+val textSize = threadHeight / 2
+val textXShift = textSize/4
 
 
-suspend fun main() = Korge(virtualSize = windowSize, windowSize = windowSize*2, backgroundColor = Colors["#2b2b2b"]) {
+suspend fun main() = Korge(virtualSize = windowSize, windowSize = windowSize, backgroundColor = Colors["#2b2b2b"]) {
     val sceneContainer = sceneContainer()
 
 
@@ -33,8 +38,8 @@ data class SelfExecutionArea(val width: Number) : ExecutionArea
 
 data class FrameExecution(val functionName: String, val areas: List<ExecutionArea>, var depth: Int = 0) : ExecutionArea
 
-const val longExecutionLen = 100.0
-const val shortExecutionLen = longExecutionLen/4
+val longExecutionLen = windowSize.width/5
+val shortExecutionLen = longExecutionLen/4
 
 
 fun MutableList<ExecutionArea>.frameExecution(functionName: String, builder: MutableList<ExecutionArea>.() -> Unit) {
@@ -83,15 +88,26 @@ fun createUiFromExecution(execution: FrameExecution): Container {
 
     val fillColor = depthColorList[execution.depth % depthColorList.size]
     container.addChildAt(text, 0)
-    RoundRect(Size(startingX, rectHeight), RectCorners(5.0), fill = fillColor, stroke = Colors.WHITE, strokeThickness = yShift/3).let {
+    RoundRect(Size(startingX, rectHeight), RectCorners(threadHeight/20), fill = fillColor, stroke = Colors.WHITE, strokeThickness = yShift/3).let {
         container.addChildAt(it, 0)
     }
     return container
 }
 
+
+fun horizontalGradientContainer(gradientSize: Size) = Container().apply {
+    val mainStrokePaint = LinearGradientPaint(0, 0, gradientSize.width, 0).addColorStop(0.0, Colors.BLACK.withAd(1.0)).addColorStop(1.0, Colors.BLACK.withAd(0.0))
+    gpuShapeView({
+        this.fill(mainStrokePaint) {
+            this.rect(0.0, 0.0, gradientSize.width, gradientSize.height)
+        }
+    }) {
+    }
+}
+
 class MyScene : Scene() {
     override suspend fun SContainer.sceneMain() {
-        val firstThreadY = 100.0
+        val firstThreadY = windowSize.width/5
 
         solidRect(windowSize.width, threadHeight, Colors.BLUEVIOLET) {
             y = firstThreadY
@@ -118,6 +134,11 @@ class MyScene : Scene() {
 
         val chunk = createUiFromExecution(execution)
         addChild(chunk)
+
+        horizontalGradientContainer(Size(windowSize.width/3, windowSize.height)).also {
+            addChild(it)
+        }
+
         val chunkWidth = chunk.width
         chunk.y = firstThreadY
         chunk.tween(chunk::x[windowSize.width, 0.0 - chunkWidth], time = 10.seconds, easing = Easing.LINEAR)
