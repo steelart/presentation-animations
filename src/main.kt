0edcb8dc90ada3844ae69d25c0041b7931309f36
Alexey.Merkulov <alexey.merkulov@jetbrains.com>
2025-02-27 20:46:13 +0100

    in-progress-15

--- src/main.kt
+++ src/main.kt
@@ -42,7 +42,7 @@ suspend fun main() = Korge(virtualSize = windowSize, windowSize = windowSize, ba
 sealed interface ExecutionArea
 
 
-data class EventAndNextRunningType(val event: TimelineEventType, val nextRunningType: RunningType)
+data class EventAndNextRunningType(val event: TimelineEventType, val nextRunningType: RunningType, val injection: ExecutionArea? = null)
 
 data class SelfExecutionArea(val width: Number, val eventAndNextRunningType: EventAndNextRunningType?) : ExecutionArea
 
@@ -73,13 +73,13 @@ fun adjustDepth(execution: FrameExecution, depth: Int) {
 
 val depthColorList = listOf(Colors.ORANGE, Colors.BLUE, Colors.DARKGREEN)
 
-data class TimelineEvent(val position: Double, val eventAndNextRunningType: EventAndNextRunningType)
+data class TimelineEvent(val absPosition: Double, val eventAndNextRunningType: EventAndNextRunningType)
 
 class CollectedInfo {
     val breakPointPositions = mutableListOf<TimelineEvent>()
 }
 
-fun CollectedInfo.createUiFromExecution(execution: FrameExecution, xTopShift: Double): Container {
+fun CollectedInfo.createUiFromExecution(execution: FrameExecution, xFirstFrameShift: Double): Container {
     val container = Container()
     val rectHeight = threadHeight - yShift*execution.depth
 
@@ -102,12 +102,12 @@ fun CollectedInfo.createUiFromExecution(execution: FrameExecution, xTopShift: Do
                             container.addChild(it)
                         }
                     }
-                    breakPointPositions.add(TimelineEvent(xTopShift + centerX, area.eventAndNextRunningType))
+                    breakPointPositions.add(TimelineEvent(xFirstFrameShift + centerX, area.eventAndNextRunningType))
                 }
                 startingX += area.width.toDouble()
             }
             is FrameExecution -> {
-                val ui = createUiFromExecution(area, xTopShift + startingX)
+                val ui = createUiFromExecution(area, xFirstFrameShift + startingX)
                 ui.x = startingX
                 ui.y = yShift/2
                 container.addChild(ui)
@@ -141,6 +141,7 @@ fun horizontalGradientContainer(gradientSize: Size, leftAlpha: Double, rightAlph
 enum class RunningType {
     Running,
     SteppingOver,
+    Evaluation,
 }
 
 
@@ -293,12 +294,48 @@ fun suspendThreadModeCase(): List<TreadUiData> {
 }
 
 
+fun coroutineCase(): List<TreadUiData> {
+    val getCoroutineInjection = FrameExecution("getCoroutineId", buildList {
+        selfExecutionArea(longExecutionLen)
+    })
+
+
+    val execution1 = FrameExecution("dispatch", buildList {
+        selfExecutionArea(longExecutionLen)
+        frameExecution("launch 1") {
+            selfExecutionArea(shortExecutionLen, EventAndNextRunningType(TimelineEventType.Breakpoint, RunningType.Evaluation, getCoroutineInjection))
+            selfExecutionArea(longExecutionLen)
+        }
+        selfExecutionArea(shortExecutionLen, TimelineEventType.SteppingEnd, RunningType.Running)
+        for (i in 0..10) {
+            selfExecutionArea(shortExecutionLen)
+            frameExecution("boo") {
+                selfExecutionArea(shortExecutionLen)
+            }
+        }
+    })
+
+    val execution2 = FrameExecution("dispatch", buildList {
+        selfExecutionArea(longExecutionLen)
+        for (i in 3..100) {
+            selfExecutionArea(shortExecutionLen)
+            frameExecution("launch $i") {
+                selfExecutionArea(longExecutionLen)
+            }
+        }
+    })
+    return listOf(
+        TreadUiData(windowSize.height/4, execution1),
+        TreadUiData(windowSize.height/4 + threadHeight*2, execution2),
+    )
+}
+
 class MyScene : Scene() {
     override suspend fun SContainer.sceneMain() {
         val threadsContainer = container()
 
-        val isSynchronous = false
-        val treadUiDataList = suspendThreadModeCase()
+        val isSynchronous = true
+        val treadUiDataList = coroutineCase()
 
         horizontalGradientContainer(Size(windowSize.width/3, windowSize.height), 1.0, 0.0).also {
             addChild(it)
@@ -336,8 +373,7 @@ class MyScene : Scene() {
             val collectedInfo = CollectedInfo()
             val chunk = collectedInfo.createUiFromExecution(execution, 0.0)
 
-            val timelineEvents =
-                collectedInfo.breakPointPositions.map { TimelineEvent(it.position, it.eventAndNextRunningType) }.toMutableList()
+            val timelineEvents = collectedInfo.breakPointPositions.toMutableList()
             allTimeLineEvents.addAll(timelineEvents)
 
             threadsContainer.addChild(chunk)
@@ -360,7 +396,7 @@ class MyScene : Scene() {
         )
 
         if (isSynchronous) {
-            allTimeLineEvents.sortBy { it.position }
+            allTimeLineEvents.sortBy { it.absPosition }
             allTimeLineEvents.add(endEvent)
         }
         else {
@@ -374,16 +410,17 @@ class MyScene : Scene() {
         coroutineScope {
             for ((treadUiData, chunk, timelineEvents) in threadInfos) {
                 launch {
-                    for (event in timelineEvents) {
+                    for (timelineEvent in timelineEvents) {
                         val start = chunk.x
-                        val end = windowSize.width / 2 - event.position
+                        val end = windowSize.width / 2 - timelineEvent.absPosition
 
                         val relativeSize = (end - start).absoluteValue / windowSize.width
 
                         chunk.tween(chunk::x[start, end], time = (relativeSize*3).seconds, easing = Easing.LINEAR)
-                        if (event.eventAndNextRunningType.event == TimelineEventType.EndOfAnimation) break
+                        val event = timelineEvent.eventAndNextRunningType.event
+                        if (event == TimelineEventType.EndOfAnimation) break
 
-                        if (event.eventAndNextRunningType.event.isPaused) {
+                        if (event.isPaused) {
                             setStateText("Paused")
 
                             val c = Circle(pauseR).apply {
@@ -395,16 +432,22 @@ class MyScene : Scene() {
                                 fill = Colors.BROWN
                             }
                             threadsContainer.addChild(c)
-                            if (event.eventAndNextRunningType.event != TimelineEventType.PermanentBreakpoint) {
+                            if (event != TimelineEventType.PermanentBreakpoint) {
                                 delay(1000)
                             }
                             else {
                                 delay(100000)
                             }
                             threadsContainer.removeChild(c)
+                            val injection = timelineEvent.eventAndNextRunningType.injection
+
+                            if (injection != null) {
+
+                            }
 
                             setStateText(
-                                when (event.eventAndNextRunningType.nextRunningType) {
+                                when (timelineEvent.eventAndNextRunningType.nextRunningType) {
+                                    RunningType.Evaluation -> "Evaluation"
                                     RunningType.Running -> "Running"
                                     RunningType.SteppingOver -> "Stepping Over"
                                 }
